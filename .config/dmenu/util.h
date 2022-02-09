0fd749bca4fd87371b6fba43cd01ad203448bda5
speedie <71722170+speediegamer@users.noreply.github.com>
2022-02-09 21:04:09 +0100

    Add files via upload

--- .config/dmenu/util.h
+++ .config/dmenu/util.h
@@ -1,8 +1,16 @@
 /* See LICENSE file for copyright and license details. */
+#include <stdint.h>
 
-#define MAX(A, B)               ((A) > (B) ? (A) : (B))
-#define MIN(A, B)               ((A) < (B) ? (A) : (B))
-#define BETWEEN(X, A, B)        ((A) <= (X) && (X) <= (B))
+extern char buf[1024];
 
-void die(const char *fmt, ...);
-void *ecalloc(size_t nmemb, size_t size);
+#define LEN(x) (sizeof (x) / sizeof *(x))
+
+extern char *argv0;
+
+void warn(const char *, ...);
+void die(const char *, ...);
+
+int esnprintf(char *str, size_t size, const char *fmt, ...);
+const char *bprintf(const char *fmt, ...);
+const char *fmt_human(uintmax_t num, int base);
+int pscanf(const char *path, const char *fmt, ...);
