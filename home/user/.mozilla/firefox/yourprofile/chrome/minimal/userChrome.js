f381255dab7ea5f5c6315d8f465052413bf181b6
speedie <71722170+speediegamer@users.noreply.github.com>
2022-03-04 14:36:21 +0000

    Create userChrome.js

---
+++ home/user/.mozilla/firefox/yourprofile/chrome/minimal/userChrome.js
@@ -0,0 +1,14 @@
+ #navigator-toolbox {
+  position: relative;
+  height: 7px;
+  margin-bottom: -5px;
+  opacity: 0;
+  overflow: hidden;
+}
+
+ #navigator-toolbox:hover {
+  height: 45px;
+  margin-bottom: 0px;
+  opacity: 1;
+  overflow: show;
+}
